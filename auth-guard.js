b9d75ac8aee5f2b69bf6c3168fe0dce96d7f40a5
palak2205gupta <palak.gupta2025@vitstudent.ac.in>
2026-02-27 04:04:21 +0530

    Added auth guard protection to dashboard and smart match

---
+++ auth-guard.js
@@ -0,0 +1,23 @@
+// auth-guard.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDKOtJ9bPWH91-stp-iow8v9f_yNRKUi3c",
+  authDomain: "rescue-bite-d5058.firebaseapp.com",
+  projectId: "rescue-bite-d5058",
+  storageBucket: "rescue-bite-d5058.firebasestorage.app",
+  messagingSenderId: "1025906539351",
+  appId: "1:1025906539351:web:e3bfbea5e18748c5531c12",
+  measurementId: "G-MYJE5S51T2"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+// If user is NOT logged in, kick them to login page
+onAuthStateChanged(auth, (user) => {
+  if (!user) {
+    window.location.href = "index.html";
+  }
+});
